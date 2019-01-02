ec1ab17070f97f0135c937d79e29eeeb8b3e1cf8
borkode <31637006+borkode@users.noreply.github.com>
2019-01-01 21:55:02 -0500

    Update cur.js

--- bot/cur.js
+++ bot/cur.js
@@ -1,34 +1,27 @@
 const Discord = require('discord.js'); 
 const bot = new Discord.Client();
-var channels = ['486690504877277194','486690518537994245','486690532601364502']
-var channelsto = ['0','0','0']
-var origGame = "?help | ?talk"
+var adminch = ['486690504877277194','486690518537994245','486690532601364502']
+var ch1 = []
+var ch2 = []
+var queue = ""
+var origGame = "New features being tested!"
 var botState;
 console.log("Starting Bot Client...")
 bot.on('ready', () => {
-    console.log("Bot is ready to be used.")
+console.log("Bot is ready to be used.")
 bot.user.setActivity(origGame);
-botState = "off"
 });
 
 function getChannel(id){
-    return channels.indexOf(id)
+    return ch1.indexOf(id)
 }
 
 function getToChannel(id){
-    return channelsto.indexOf(id)
+    return ch2.indexOf(id)
 }
 
-function sendAttachmentsIfAny(attachmentsarray,mc){
-    if(attachmentsarray.length!=undefined){
-      //mc.send("",{file:attachmentsarray.MessageAttachment.url})
-      mc.send("They also attached a file.\nFiles are not supported yet.")
-      mc.send(JSON.parse(attachmentsarray).MessageAttachment.url)
-    }
-}
-
-function sendEmbed(author,messagecontent,mc){
-    if(messagecontent==""){var ctnt="This message is blank or contained a file."}else{var ctnt=messagecontent}
+function sendEmbed(author,msg,mc){
+    var ctnt = msg.content
     mc.send({embed: {
         color: 5373789,
         author: {
@@ -36,57 +29,125 @@ function sendEmbed(author,messagecontent,mc){
           icon_url: author.avatarURL
         },
         timestamp: new Date(),
-        description: ctnt+"\n",
+        description: ctnt,
         footer: {
           icon_url: bot.user.avatarURL,
           text: "powered by cool guy"
         }
       }
     });
+    console.log(msg.attachments.array()+"\n\n"+msg.attachments.array().url)
+    if(msg.attachments.array().url!=undefined){
+    mc.send({file:msg.attachments.array().url})
+    }
 }
 
+function sendAlert(alert,channel){
+    channel.send({embed: {
+        color: 5373789,
+        author:{
+            name: "Alert",
+            icon_url: bot.user.avatarURL
+        },
+        timestamp: new Date(),
+        description:alert,
+        footer: {
+            icon_url: bot.user.avatarURL,
+            text: "powered by cool guy"
+        }
+    }})
+}
+
+bot.on("guildCreate", (guild) => {
+    let channelID;
+    let channels = guild.channels;
+    channelLoop:
+    for (let c of channels) {
+        let channelType = c[1].type;
+        if (channelType === "text") {
+            channelID = c[0];
+            break channelLoop;
+        }
+    }
+
+    let channel = bot.channels.get(guild.systemChannelID || channelID);
+    setTimeout(function(){
+    channel.send({embed: {
+        color: 5373789,
+        author: {
+          name: "cool guy",
+          icon_url: bot.user.avatarURL
+        },
+        title: "Hi! I'm cool guy!",
+        description: "I am a cross-server interaction bot. Use the command `?talk` to get started! You can also use `?help` to see my other commands.",
+        footer: {
+          icon_url: bot.user.avatarURL,
+          text: "Thanks for adding me to your server!"
+        }
+      }
+    });
+},10);
+});
+
 bot.on('message', message => {
-    if(message.author.id!=486254127135981569){
+    if(!message.author.bot){
     // User commands
     if(message.content=="?talk"){
-        message.channel.send("Okay, we're connecting you to a person right now.")
-        bot.channels.get('486607994063421445').send("User "+message.author.username+" would like to chat.\n`?connect "+message.channel.id+"`")
+        if(queue!=message.channel.id.toString()){
+        sendAlert("Your server is now in the queue.",message.channel);
+        if(queue==""){
+        queue=message.channel.id.toString();
+        }else{
+        sendAlert("Connection established! Say hello!",message.channel);
+        sendAlert("Connection established! Say hello!",bot.channels.get(queue))
+        ch1.push(queue)
+        ch2.push(message.channel.id.toString())
+        queue=""
+        }
+    }else{
+        sendAlert("Your server is already on the queue! You can use the command `?unqueue` to take it off the queue.",message.channel)
+        }
     }
-    
-    // Connection command
-    if(getChannel(message.channel.id)!=-1){
-        if(message.content.startsWith('?connect')){
-            var channel = message.content.substr(9,message.content.length)
-            channelsto[getChannel(message.channel.id)]=channel.toString()
-            bot.channels.get(channel).send("You've been connected. Say hi!")
+
+    if(message.content=="?unqueue"){
+        if(queue==message.channel.id){
+            queue=""
+            sendAlert("Okay, your bot was removed from the queue.")
+        }else{
+            sendAlert("Your server is not on the queue. You can use the command `?talk` to enlist in it.",message.channel)
         }
     }
 
     //Messaging
-if(!message.content.startsWith('?connect') && !message.content.startsWith("?disconnect")){
-    // Client to Us
+if(!message.content.startsWith('?help') && !message.content.startsWith("?disconnect") && !message.content.startsWith('?bugreport') && !message.content.startsWith("?talk")){
+    // ch2 to ch1
     if(getToChannel(message.channel.id)!=-1){
-        sendEmbed(message.author,message.content,bot.channels.get(channels[getToChannel(message.channel.id)]))
-        sendAttachmentsIfAny(message.attachments,message.channel)
+        sendEmbed(message.author,message,bot.channels.get(ch1[getToChannel(message.channel.id)]))
         
     }
-    // Us to Client
+    // ch1 to ch2
     if(getChannel(message.channel.id)!=-1){
-        sendEmbed(message.author,message.content,bot.channels.get(channelsto[getChannel(message.channel.id)]))
-        sendAttachmentsIfAny(message.attachments,message.channel)
+        sendEmbed(message.author,message,bot.channels.get(ch2[getChannel(message.channel.id)]))
     }}
     //Disconnection command
         if(message.content=='?disconnect'){
     if(getChannel(message.channel.id)!=-1){
-        bot.channels.get(channels[getChannel(message.channel.id)]).send("The other person has disconnected. If you wish to talk again, use `?talk`.")
-        bot.channels.get(channelsto[getChannel(message.channel.id)]).send("You have disconnected from the other person. If you wish to talk again, use `?talk`.")
-        channelsto[getChannel(message.channel.id)]='0'
+        sendAlert("The other person has disconnected. If you wish to talk again, use `?talk`.",bot.channels.get(ch1[getChannel(message.channel.id)]))
+        sendAlert("You have disconnected from the other person. If you wish to talk again, use `?talk`.",bot.channels.get(ch2[getChannel(message.channel.id)]))
+        ch1[getChannel(message.channel.id)]='0'
+        ch2[getChannel(message.channel.id)]='0'
     }
     if(getToChannel(message.channel.id)!=-1){
-        bot.channels.get(channels[getToChannel(message.channel.id)]).send("The other person has disconnected. If you wish to talk again, use `?talk`.")
-        bot.channels.get(channelsto[getToChannel(message.channel.id)]).send("You have disconnected from the other person. If you wish to talk again, use `?talk`.")
-        channelsto[getToChannel(message.channel.id)]='0'
+        sendAlert("The other person has disconnected. If you wish to talk again, use `?talk`.",bot.channels.get(ch2[getChannel(message.channel.id)]))
+        sendAlert("You have disconnected from the other person. If you wish to talk again, use `?talk`.",bot.channels.get(ch1[getChannel(message.channel.id)]))
+        ch1[getToChannel(message.channel.id)]='0'
+        ch2[getToChannel(message.channel.id)]='0'
     }
+    }
+    // Bug report command
+    if(message.content.startsWith("?bugreport ")){
+        sendEmbed(message.author,message.substring(10,message.content.length)+"\n\n**"+message.author.id+"**",bot.channels.get('529541510895632394'));
+        sendAlert("Thank you for the bug report!",message.channel);
     }
     //Help command
     if(message.content=="?help"){
@@ -100,15 +161,19 @@ if(!message.content.startsWith('?connect') && !message.content.startsWith("?disc
             description: "Brings up this menu",
             fields: [{
                 name: "?talk",
-                value: "Enlists your server in the queue to talk to a random server"
-              },
-              {
-                name: "?connect {Server ID}",
-                value: "Connects you to a specific server using the server ID"
+                value: "Enlists your server in the queue to talk to another random server"
               },
               {
                 name: "?disconnect",
-                value: "Disconnects you from any connection (if any)"
+                value: "Removes your server from the connection (if any)"
+              },
+              {
+                name: "?unqueue",
+                value: "Unenlists you from the queue, if your server is on there."
+              },
+              {
+                name: "?bugreport {Bug/Explanation}",
+                value: "Find a bug? Submit it! We can't find all the bugs ourselves!"
               }
             ],
             footer: {
@@ -121,4 +186,4 @@ if(!message.content.startsWith('?connect') && !message.content.startsWith("?disc
 }
 });
 
-// Don't forget to use your bot.login(); here.
+bot.login(token);
